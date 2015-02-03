086c42f7b477630cdae4e60d8f3071cc18f304ea
koontz <koontz@cs.colostate.edu>
2015-02-03 13:34:21 -0700

    making rectangles appear from d3, rather than leaflet library. Much easier, because we can use d3.json for both.

--- jsonpCallBack.js
+++ jsonpCallBack.js
@@ -1,16 +1,15 @@
 var latLons = [];
 var type = "";
-function setLatLonValuesAndType( latLonsFromLeaflet , typeFromLeaflet){
-    if(latLons.length != 0){
+function setLatLonValuesAndType(latLonsFromLeaflet, typeFromLeaflet) {
+    if (latLons.length != 0) {
         //there was a query area that was edited, remove old latLons and accept new one.
-        while(latLons.length > 0) {
+        while (latLons.length > 0) {
             latLons.pop();
         }
     }
     latLons = latLonsFromLeaflet;
     type = typeFromLeaflet;
 }
-
 
 
 function sendRequest() {
