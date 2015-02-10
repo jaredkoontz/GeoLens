53dfdbfebfc1d5a0d1477df7dd30eb6aeed34c52
koontz <koontz@cs.colostate.edu>
2015-02-10 14:10:17 -0700

    sample json file

--- hist.js
+++ hist.js
@@ -1,12 +1,3 @@
-var fakeBarData = [
-    {'x': "1", 'y': 5},
-    {'x': "20", 'y': 20},
-    {'x': "40", 'y': 10},
-    {'x': "60", 'y': 40},
-    {'x': "80", 'y': 5},
-    {'x': "100", 'y': 60}
-];
-
 var windowHeight = $(document).height(); // returns height of HTML document
 var windowWidth = $(document).width(); // returns width of HTML document
 
